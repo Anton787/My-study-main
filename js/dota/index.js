63eebcf1cfea02d68abdec3ff295559a9648423d
Anton787 <antongagelganc6443@gmail.com>
2024-02-21 04:43:32 +0500

    dota2 (без нормальных картинок и текста)

--- js/dota/index.js
+++ js/dota/index.js
@@ -1,15 +1,30 @@
-function see(i) {
-    const div_www = document.querySelector(".www")
-    if (div_www != null){
-        div_www.classList.remove("www")
+function see(i,x) {
+    if (x[0] == 0){console.log(x)
+        div_content.classList.add("to-left")
+        discrip[i].classList.add("www")
+        x[0] = 1
+        console.log('выполнил 1yui ' + x + ' ' + i)
     }
-    discrip[i].classList.add("www")
+    else{
+        const div_www = document.querySelector(".www")
+        if (div_www != null){
+            div_www.classList.remove("www")
+        }
+        const div_ww = document.querySelector(".ww")
+        if (div_ww != null){
+            div_ww.classList.remove("ww")
+        }
+        discrip[i].classList.add("ww")
+        console.log('выполнил 2')
+    }
 }
 
+var x = [0]
+const div_content = document.getElementById("1")
 const div = document.querySelectorAll(".place");
 const discrip = document.querySelectorAll(".description")
 
 
 for (let i = 0; i<div.length; i++){
-    div[i].addEventListener("click", ()=>{see(i)})
+    div[i].addEventListener("click", ()=>{see(i,x)})
 };
