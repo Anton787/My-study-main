8ece0203ad64c0f3981c16c90cd0cf72f940fdc1
Anton787 <antongagelganc6443@gmail.com>
2024-03-14 22:38:45 +0500

    Strapi api "post"

--- js/async copy/index.js
+++ js/async copy/index.js
@@ -29,7 +29,65 @@
 // создать функцию которая при нажитии на button делает пост 
 
 const xml = new XMLHttpRequest();
+const loadPanel = document.querySelector('.loading');
+const contentPanel = document.querySelector('.content');
+const button = document.querySelector('button');
+var contentHeight = document.querySelector('.content').clientHeight;
+var data;
 
-const getPosts = new Promise {
-  xml.open("GET", '')
+function seePosts() {
+  contentPanel.style.display = 'block'
+  loadPanel.style.display = 'none'
 }
+
+function createdPost (){
+
+}
+
+const getPosts = new Promise ((resolve, reject) => {
+  xml.open("GET", 'http://localhost:1337/api/posts');
+
+  xml.send();
+  contentPanel.style.display = 'none'
+  loadPanel.style.display = 'block';
+  loadPanel.style.height = contentHeight;
+  xml.addEventListener("load", () => {
+    if(xml.status == 200){
+      resolve(xml.response);
+    }
+    else{
+      reject(xml.status)
+    }
+  });
+
+});
+
+
+getPosts.then((response) => {
+  setTimeout(seePosts, 1000);
+  var response = JSON.parse(response);
+  const data = response.data;
+  data.forEach((e) => {
+    createdPost(e.attributes)
+  });
+  console.log(response.data);
+}).catch((status) => {
+  console.log(status);
+});
+
+function createdPost(data) {
+  var title = data.title || "Если не будет";
+  var text = data.text || "Буквы";
+  var divPost = document.createElement('div');
+  var postTitle = document.createElement('h3');
+  var textPost = document.createElement('p');
+  postTitle.innerText = title;
+  textPost.innerText = text;
+
+  divPost.appendChild(postTitle)
+  divPost.appendChild(textPost)
+  contentPanel.appendChild(divPost)
+  console.log(title, text)
+}
+
+button.addEventListener('click', createdPost);
